034da2e72190ad6c7d067451e21c5c2f5b30e9f3
NNECHI ANGELA UCHECHI <angelannechi@gmail.com>
2025-02-18 20:02:48 +0100

    finished desktop

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 "use script";
 document.querySelector(".menu-icon").addEventListener("click", function () {
-  const menuLinks = document.querySelector(".nav-bar");
+  const menuLinks = document.querySelector(".welcome-nav");
   menuLinks.classList.toggle("show");
   const icon = document.querySelector("#barz");
 });
