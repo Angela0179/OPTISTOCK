00d05512725116bcdd9c7c0c56936fd63cbf26ed
NNECHI ANGELA UCHECHI <angelannechi@gmail.com>
2025-02-23 12:40:11 +0100

    correction to sign up and login page

--- JS/main.js
+++ JS/main.js
@@ -69,3 +69,18 @@ const handleBackspace = (inputNumber, event) => {
   }
 };
 // input field validation*/
+const myName = document.getElementById("name");
+const password = document.getElementById("password");
+form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (myName.value === "" || myName.value == null) {
+    messages.push("Name /email is required");
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(",");
+  }
+});
