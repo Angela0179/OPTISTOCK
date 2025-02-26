56dc9fab69d1673df4c4208d50329af8200c21b9
Grace <gracegodson.o@gmail.com>
2025-02-26 18:07:37 +0100

    Did some more modifications including adding the active class to all nav bars using javascript

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-document.querySelector("menu-icon").addEventListener("click", function(){
-    const menuLinks = document.querySelector("#menu-Links");
+//js for a responsive menu links//
+document.querySelector(".menu-icon").addEventListener("click", function(){
+    const menuLinks = document.querySelector("#menu-links");
     const icon = document.querySelector("#bars");
     icon.classList.toggle("fa-xmark");
-    menuLinks.classList.toggle("show");
+    menuLinks.classList.add(".show")
+})
+
+//script for an active nav bar
+document.addEventListener("DOMContentLoaded", function(){
+    let currentLocation = window.location.href;
+    let menuItems = document.querySelectorAll("nav ul li a");
+    menuItems.forEach(item => {
+        if (item.href === currentLocation){
+            item.classList.add("active")
+        }
+    })
 })
